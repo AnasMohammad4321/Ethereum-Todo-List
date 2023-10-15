a4367fdc6b3cd93e5494736d67a80ffaa105401d
Mohammad Anas <mohammadanas702@gmail.com>
2023-10-15 22:26:12 +0500

    TodoList-Contract

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,22 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
+    mapping(uint => Task) public tasks;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    constructor() public {
+        createTask("Sample Task");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+
+    }
 }
+
